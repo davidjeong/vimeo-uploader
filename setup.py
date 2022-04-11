8a410a2ef41327c5b1531d2390439c7183e5f4f1
David Jeong <djeong747@gmail.com>
2022-04-11 16:28:26 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vimeo_uploader',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='David Jeong',
+    author_email='djeong747@gmail.com',
+    description='Tool for downloading YouTube video, trim, and re-upload to Vimeo'
+)
