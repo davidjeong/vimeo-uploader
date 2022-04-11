9ee2b2de1232585b3114d826816e5e56e435e95a
David Jeong <djeong747@gmail.com>
2022-04-11 16:14:38 -0400

    Make it a bit more modular

---
+++ video_configuration.py
@@ -0,0 +1,18 @@
+class VideoConfiguration:
+
+    def __init__(self, video_url, start_time_in_sec, end_time_in_sec, resolution, video_title=None, image_url=None):
+        """
+        Initialize the configuration object
+        :param video_url: URL to video
+        :param start_time_in_sec: Start time of video in seconds
+        :param end_time_in_sec: End time of video in seconds
+        :param resolution: Resolution of the video
+        :param video_title: Title of the video
+        :param image_url: URL to the thumbnail image
+        """
+        self.video_url = video_url
+        self.start_time_in_sec = start_time_in_sec
+        self.end_time_in_sec = end_time_in_sec
+        self.resolution = resolution
+        self.video_title = video_title
+        self.image_url = image_url
