07bb32f277daf3758e47a4b805bb7a1a0efd109b
David Jeong <djeong747@gmail.com>
2022-05-01 19:13:17 -0400

    Try making test pass

--- tests/core/test_driver.py
+++ tests/core/test_driver.py
@@ -17,8 +17,8 @@ def test_download_youtube_resources_and_combine(tmpdir) -> None:
     test_url = 'https://www.youtube.com/watch?v=XsX3ATc3FbA'
     test_resolution = '1080p'
 
-    tmp_video_path = get_absolute_path(tmpdir, 'v.mp4')
-    tmp_audio_path = get_absolute_path(tmpdir, 'a.mp3')
+    tmp_video_path = get_absolute_path(tmpdir, 'video_stream.mp4')
+    tmp_audio_path = get_absolute_path(tmpdir, 'audio_stream.mp3')
 
     download_youtube_resources(
         tmpdir,
@@ -35,8 +35,8 @@ def test_download_youtube_resources_and_combine(tmpdir) -> None:
     audio = MP4(tmp_audio_path)
     assert int(audio.info.length) == 252
 
-    combined_path = get_absolute_path(tmpdir, "c.mp4")
-    final_path = get_absolute_path(tmpdir, "f.mp4")
+    combined_path = get_absolute_path(tmpdir, "combined_video.mp4")
+    final_path = get_absolute_path(tmpdir, "final_video.mp4")
     join_resource(tmp_video_path, tmp_audio_path, combined_path)
 
     assert exists(combined_path)
@@ -50,8 +50,6 @@ def test_download_youtube_resources_and_combine(tmpdir) -> None:
     video = VideoFileClip(final_path)
     assert int(video.duration) >= 60 or int(video.duration) <= 61
     assert int(video.audio.duration) >= 60 or int(video.duration) <= 61
-
-    print("Final video in " + final_path)
 
 
 def test_upload_video_to_vimeo() -> None:
