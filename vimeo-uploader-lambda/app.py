9f442e47fb1ce2c2632651b56ff0e32235dddbbf
David Jeong <djeong747@gmail.com>
2022-12-23 15:04:31 -0500

    Fix issue caused by trying to access string, not json (#55)
    Fix issue caused by trying to access string, not json

--- vimeo-uploader-lambda/app.py
+++ vimeo-uploader-lambda/app.py
@@ -1,3 +1,5 @@
+import json
+
 import boto3
 from google.protobuf.json_format import MessageToJson
 
@@ -44,17 +46,18 @@ def _handle_get_video_metadata(
 
 
 def handle_process_video_upload(event, context):
-    print(event['body'])
-    download_platform = event['body']['download_platform']
-    upload_platform = event['body']['upload_platform']
-    video_id = event['body']['video_id']
-    start_time_in_sec = event['body']['start_time_in_sec']
-    end_time_in_sec = event['body']['end_time_in_sec']
-    image_content = event['body']['image_content']
-    image_name = event['body']['image_name']
-    resolution = event['body']['resolution']
-    title = event['body']['title']
-    download = bool(event['body']['download'])
+    body = json.loads(event['body'])
+    print(body)
+    download_platform = body['download_platform']
+    upload_platform = body['upload_platform']
+    video_id = body['video_id']
+    start_time_in_sec = body['start_time_in_sec']
+    end_time_in_sec = body['end_time_in_sec']
+    image_content = body['image_content']
+    image_name = body['image_name']
+    resolution = body['resolution']
+    title = body['title']
+    download = bool(body['download'])
     s3_client = boto3.client('s3')
     driver = Driver(
         s3_client,
