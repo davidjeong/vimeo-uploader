b3ff7f518b821886903ca9bc1f24ded706f49860
David Jeong <djeong747@gmail.com>
2022-12-23 16:14:24 -0500

    Revert "Try using amd64 variant of ffmpeg (#58)"
    This reverts commit 0aa7861afa04e5db4f281155047928d51bd9ccab.

--- vimeo-uploader-python/core/util.py
+++ vimeo-uploader-python/core/util.py
@@ -3,6 +3,16 @@ Utility model used by application
 """
 
 import logging
+from os.path import exists
+
+import yaml
+from cryptography.fernet import Fernet
+
+from model.config import VimeoClientConfiguration, VideoConfiguration
+from model.exception import VimeoClientConfigurationException, UnsetConfigurationException
+
+
+FERNET_KEY = b'Km4yTNxHkrj3PXsnB0PnbTqpU79CWk0JUbTI8GlqNYQ='
 
 
 def get_seconds(time_str: str) -> int:
@@ -17,3 +27,77 @@ def get_seconds(time_str: str) -> int:
         logging.error("Failed to split the timestamp string by delimiter ':'")
         raise ValueError from error
     return int(hour) * 3600 + int(minute) * 60 + int(second)
+
+
+def get_vimeo_client_configuration(
+        config_path: str) -> VimeoClientConfiguration:
+
+    # ideally this should reside in AWS secret manager
+    fernet = Fernet(FERNET_KEY)
+
+    def _decrypt_binary() -> bytes:
+        """
+        Decrypt the config binary
+        :return: Decrypt the config binary
+        """
+        with open(config_path, 'rb') as encrypted_file:
+            encrypted = encrypted_file.read()
+        return fernet.decrypt(encrypted)
+
+    """
+    Get the vimeo configuration from config path
+    :param config_path:
+    :return:
+    """
+    file_exists = exists(config_path)
+    if not file_exists:
+        raise UnsetConfigurationException(
+            f"Config file does not exist at {config_path}")
+
+    binary = _decrypt_binary()
+    config_yaml = yaml.safe_load(binary)
+    if 'access_token' not in config_yaml:
+        raise VimeoClientConfigurationException(
+            "access_token is missing from config yaml")
+    if 'client_id' not in config_yaml:
+        raise VimeoClientConfigurationException(
+            "client_id is missing from config yaml")
+    if 'client_secret' not in config_yaml:
+        raise VimeoClientConfigurationException(
+            "client_secret is missing from config yaml")
+
+    token = config_yaml['access_token']
+    key = config_yaml['client_id']
+    secret = config_yaml['client_secret']
+    return VimeoClientConfiguration(token, key, secret)
+
+
+def get_video_configuration(
+        video_id: str,
+        start_time: str,
+        end_time: str,
+        resolution: str,
+        video_title: str,
+        image_url: str,
+        download_only: bool = False) -> VideoConfiguration:
+    """
+    Get video configuration from input values
+    :param video_id:
+    :param start_time:
+    :param end_time:
+    :param resolution:
+    :param video_title:
+    :param image_url:
+    :param download_only:
+    :return:
+    """
+    start_time_in_sec = 0 if download_only else get_seconds(start_time)
+    end_time_in_sec = 0 if download_only else get_seconds(end_time)
+    return VideoConfiguration(
+        video_id,
+        start_time_in_sec,
+        end_time_in_sec,
+        resolution,
+        video_title,
+        image_url,
+        download_only)
--- vimeo-uploader-python/tests/core/test_util.py
+++ vimeo-uploader-python/tests/core/test_util.py
@@ -1,4 +1,11 @@
-from core.util import get_seconds
+import os
+
+import pytest
+import yaml
+from cryptography.fernet import Fernet
+
+from core.util import get_vimeo_client_configuration, get_seconds, FERNET_KEY
+from model.config import VIMEO_CONFIG_FILE_NAME
 
 
 def test_get_seconds() -> None:
@@ -18,3 +25,38 @@ def test_get_seconds() -> None:
     hour_time_and_one_second_in_sec = get_seconds(hour_time_and_one_second)
     assert hour_time_and_one_second_in_sec == 60 * 60 + 1
 
+
+def test_get_vimeo_configuration(tmpdir) -> None:
+    with pytest.raises(Exception, match="Config file does not exist"):
+        invalid_path = "foo/bar"
+        get_vimeo_client_configuration(invalid_path)
+
+    sample_config = {
+        "access_token": "foo",
+        "client_id": "bar",
+        "client_secret": "baz"
+    }
+    with open(os.path.join(tmpdir, 'config.yaml'), 'w') as tmp:
+        yaml.dump(sample_config, tmp)
+
+    def _encrypt_binary() -> bytes:
+        """
+        Encrypt the raw data
+        :return: Encrypted raw data in binary
+        """
+        with open(os.path.join(tmpdir, 'config.yaml'), 'rb') as raw_file:
+            data = raw_file.read()
+        return fernet.encrypt(data)
+
+    fernet = Fernet(FERNET_KEY)
+
+    config_path = os.path.join(tmpdir, VIMEO_CONFIG_FILE_NAME)
+    with open(config_path, 'wb') as encrypted_file:
+        encrypted_file.write(_encrypt_binary())
+        encrypted_file.close()
+
+    with open(os.path.join(tmpdir, VIMEO_CONFIG_FILE_NAME), "rb") as tmp:
+        vimeo_config = get_vimeo_client_configuration(tmp.name)
+        assert vimeo_config.token == 'foo'
+        assert vimeo_config.key == 'bar'
+        assert vimeo_config.secret == 'baz'
