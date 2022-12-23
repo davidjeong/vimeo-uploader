92f8b4e2b65b758d2e46e3e850061ee10748c418
David Jeong <djeong747@gmail.com>
2022-12-22 20:47:03 -0500

    Refactor driver code for testing (#49)
    - Refactor a lot of code to allow for easy testing (inversion of control)
    - Move tests from test_app.py to test_driver.py

--- vimeo-uploader-lambda/app.py
+++ vimeo-uploader-lambda/app.py
@@ -1,18 +1,22 @@
-import os
-
 import boto3
 from google.protobuf.json_format import MessageToJson
 
-from core.driver import Driver
+from core.driver import Driver, get_streaming_platform
 from core.exceptions import VimeoUploaderInternalServerError, VimeoUploaderInvalidVideoIdError
 
 
 def handle_get_video_metadata(event, context):
-    driver = Driver()
     platform = event['queryStringParameters']['platform']
     video_id = event['queryStringParameters']['video_id']
+    driver = Driver(download_platform=get_streaming_platform(platform))
+    return _handle_get_video_metadata(driver, video_id)
+
+
+def _handle_get_video_metadata(
+        driver: Driver,
+        video_id: str):
     try:
-        video_metadata = driver.get_video_metadata(platform, video_id)
+        video_metadata = driver.get_video_metadata(video_id)
         print(f"Retrieved the video metadata for video id {video_id}")
         return {
             'headers': {
@@ -39,9 +43,6 @@ def handle_get_video_metadata(event, context):
 
 
 def handle_process_video_upload(event, context):
-    driver = Driver(
-        s3_client=boto3.client('s3'),
-        environment=os.environ['ENV'])
     download_platform = event['download_platform']
     upload_platform = event['upload_platform']
     video_id = event['video_id']
@@ -52,10 +53,35 @@ def handle_process_video_upload(event, context):
     resolution = event['resolution']
     title = event['title']
     download = bool(event['download'])
+    s3_client = boto3.client('s3')
+    driver = Driver(
+        s3_client,
+        get_streaming_platform(download_platform),
+        get_streaming_platform(upload_platform))
+    return _handle_process_video_upload(
+        driver,
+        video_id,
+        start_time_in_sec,
+        end_time_in_sec,
+        image_content,
+        image_name,
+        resolution,
+        title,
+        download)
+
+
+def _handle_process_video_upload(
+        driver: Driver,
+        video_id: str,
+        start_time_in_sec: int,
+        end_time_in_sec: int,
+        image_content: bytes,
+        image_name: str,
+        resolution: str,
+        title: str,
+        download: bool):
     try:
         video_process_result = driver.process_video(
-            download_platform,
-            upload_platform,
             video_id,
             start_time_in_sec,
             end_time_in_sec,
