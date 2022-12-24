2fc45afd1f3028eb3f91a4d280f03c635e7c8e31
David Jeong <djeong747@gmail.com>
2022-12-24 01:07:32 -0500

    Initial commit for swift client

---
+++ vimeo-uploader-swift/vimeo-uploader-swiftUITests/vimeo_uploader_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  vimeo_uploader_swiftUITestsLaunchTests.swift
+//  vimeo-uploader-swiftUITests
+//
+//  Created by David Jeong on 12/24/22.
+//
+
+import XCTest
+
+final class vimeo_uploader_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
