3c21e4301a6fb72931aa68a10af8654e64fd0c96
David Jeong <djeong747@gmail.com>
2022-12-24 00:59:45 -0500

    Initial commit for vimeo-uploader-swift

---
+++ vimeo-uploader-swift/vimeo-uploader-swift/vimeo_uploader_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  vimeo_uploader_swiftApp.swift
+//  vimeo-uploader-swift
+//
+//  Created by David Jeong on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct vimeo_uploader_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
