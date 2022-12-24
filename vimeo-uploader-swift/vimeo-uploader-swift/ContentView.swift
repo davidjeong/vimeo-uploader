fe5f2350dc1d7bfc13d5d37ab85694793e46bd75
David Jeong <djeong747@gmail.com>
2022-12-24 14:52:49 -0500

    Create ui elements for mac client (#66)

--- vimeo-uploader-swift/vimeo-uploader-swift/ContentView.swift
+++ vimeo-uploader-swift/vimeo-uploader-swift/ContentView.swift
@@ -8,12 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    
+    @State private var videoId: String = ""
+    @State private var startTime: String = ""
+    @State private var endTime: String = ""
+    @State private var resolution = ""
+    @State private var title = ""
+    @State private var download = false
+    
+    let sampleResolutions = ["240p", "360p", "480p", "640p", "720p", "1080p"].reversed()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Grid {
+            GridRow {
+                Text("Video ID")
+                TextField("e.g. XsX3ATc3FbA", text: $videoId)
+            }.padding()
+            Divider()
+            Group {
+                GridRow {
+                    Text("Video Start Time")
+                    TextField("e.g. 00:45:10", text: $startTime)
+                }.padding()
+                Divider()
+                GridRow {
+                    Text("Video End Time")
+                    TextField("e.g. 01:30:10", text: $endTime)
+                }.padding()
+            }
+            Divider()
+            Group {
+                GridRow {
+                    Text("Video Resolution")
+                    Picker("", selection: $resolution) {
+                        ForEach(sampleResolutions, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }.padding()
+                Divider()
+                GridRow {
+                    Text("Video Title")
+                    TextField("e.g. BTS music video", text: $title)
+                }.padding()
+            }
+            Divider()
+            GridRow {
+                Text("Save")
+                Toggle("Download On Completion", isOn: $download).toggleStyle(.checkbox)
+            }.padding()
+            Divider()
+            GridRow {
+                Text("Process")
+                Button("Click to start the process", action: {
+                    print("Start process")
+                })
+            }.padding()
         }
         .padding()
     }
